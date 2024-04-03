dbfdd38317a2539caf0b9fc63ab89252fb2a7318
OMAL S <95520329+omals@users.noreply.github.com>
2024-04-04 00:15:10 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -15,7 +15,7 @@ export const Bio = {
     twitter: "https://x.com/omal_shivan?t=794J-I90y1zL1cVH-BeRfA&s=09",
     insta: "https://www.instagram.com/omal_shivan/",
     facebook: "https://www.facebook.com/profile.php?id=100071666576910&mibextid=ZbWKwL",
-    photo: "https://drive.google.com/file/d/1EhbhpM_-uegxgMDnXNoIUmrPnYnx2JiA/view?usp=drivesdk", 
+    photo: "https://avatars.githubusercontent.com/u/95520329?v=4", 
   };
   
   export const skills = [
